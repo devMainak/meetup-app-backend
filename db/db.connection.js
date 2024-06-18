cb7ef383739d77e3e018c4f9a84bfc2dabb3f30d
rootKid <137388933+devMainak@users.noreply.github.com>
2024-06-18 10:07:24 +0000

    db connected

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose")
 
 const mongoURI = process.env['MONGODB']
 
-const initializeDatabase = () => {
-  
+const initializeDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(mongoURI)
+    if (connection) {
+      console.log("Connected Successfully.")
+    }
+  } catch (error) {
+    throw error
+  }
 }
+
+module.exports = initializeDatabase
